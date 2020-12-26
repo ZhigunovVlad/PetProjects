28350840fe7e6ab18f7136783dda1c04af8ac781
Vladislav <69080027+ZhigunovVlad@users.noreply.github.com>
2020-12-26 15:58:16 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-    Palindrom palindrom=new Palindrom();
-        System.out.println(palindrom.isPalindrom("ababa")); //true
-        System.out.println(palindrom.isPalindrom("hello")); //false
-        System.out.println(palindrom.isPalindrom("12321")); //true
-        System.out.println(palindrom.isPalindrom("Sqllqs"));  //false
-    }
-}
